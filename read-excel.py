180726f71dee16dae803ccee543575ff41370c37
Richard Crooks <email@rocrooks.co.uk>
2019-05-28 12:46:47 +0100

    Writing to Excel file

--- read-excel.py
+++ read-excel.py
@@ -1,11 +1,21 @@
 #Import required modules
 import pandas as pd
 
+#Get the Excel file and the list of sheets which it contains
 excel = pd.ExcelFile('example-excel.xlsx')
-
 sheets = excel.sheet_names
-
 print(sheets)
+
+#Test dataframe
+df1 = pd.DataFrame([['a', 'b'], ['c', 'd']],
+#index=['row 1', 'row 2'],
+#columns=['col 1', 'col 2']
+)
+
+df1.to_excel("output.xlsx",
+header = False,
+index = False
+)
 
 #df = pd.read_excel(r'example-excel.xlsx')
 #print(df)
